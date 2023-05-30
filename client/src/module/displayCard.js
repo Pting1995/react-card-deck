8cfb676af417dccfa13c70e6d6133d1efa7548e2
Peter Ting <pting1995@gmail.com>
2023-05-30 16:13:03 -0700

    added card state buttons

--- client/src/module/displayCard.js
+++ client/src/module/displayCard.js
@@ -1,9 +1,23 @@
-function displayCard(props) {
+import React, { useState } from "react";
+
+function DisplayCard(props) {
+	const [cardState, setCardState] = useState("deck")
+
+	const addToHand = () => {
+		setCardState("hand")
+	}
+	const discardCard = () => {
+		setCardState("discard")
+	}
+
 	return (
 		<div className="card-container">
-			<img src={props.url} name={props.name} class="card"></img>
+			<p>{cardState}</p>
+			<img src={props.url} alt={props.name} className="card"></img>
+			<button onClick={addToHand}>Draw</button>
+			<button onClick={discardCard}>Discard</button>
 		</div>
 	);
 }
 
-export default displayCard;
+export default DisplayCard;
