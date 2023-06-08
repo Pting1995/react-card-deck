b9a96d1f765bc1c4115acaf22c2215cff1acc23b
Peter Ting <pting1995@gmail.com>
2023-06-08 09:56:50 -0700

    added settings sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import DisplayDeck from "./module/displayDeck"
+import SettingsPanel from "./module/settingsPanel"
 
 function App() {
   return (
-    <div className="overview-container">
-      <DisplayDeck />
+    <div className="app-container">
+      <div className="settings-container">
+        <SettingsPanel />
+      </div>
+      <div className="play-mat-container">
+        <DisplayDeck />
+      </div>
     </div>
+
   );
 }
 
---
+++ client/src/module/settingsPanel.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+// a.shuffle
+
+// b.show / hide hand, deck, discarded
+// c.draw 1 random card
+// d.discard 1 random card
+// e.reset deck button
+
+function SettingsPanel() {
+
+	return (
+		<div className="settings">
+			<p>hi</p>
+		</div>
+	);
+}
+
+export default SettingsPanel;
